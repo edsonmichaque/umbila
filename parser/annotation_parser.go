3978351e41fd926345e1d585e23da86f903e1e89
Edson Michaque <edson@michaque.com>
2024-05-04 21:32:46 +0200

    feat: implement annotation params parsing

--- parser/annotation_parser.go
+++ parser/annotation_parser.go
@@ -18,6 +18,13 @@ type AnnotationValue struct {
 	String *string
 	Number *string
 	Bool   *string
+	Params []*assignmentDef
+}
+
+type AnnotationParam struct {
+	Token
+	Name  string
+	Value string
 }
 
 func ParseAnnotation(p Tokenizer) (Def, error) {
@@ -42,6 +49,28 @@ func parseAnnotation(tok Tokenizer) (*AnnotationDef, error) {
 		tok.NextToken()
 
 		switch tok.PeekToken().Type {
+		case TypeIdent:
+
+			if ann.Value.Params == nil {
+				ann.Value.Params = make([]*assignmentDef, 0)
+			}
+
+			for tok.PeekToken().Type != TypeRParen {
+				tok.NextToken()
+
+				log.Println("Parsing assignment")
+				def, err := parseAssignment(tok)
+				if err != nil {
+					return nil, err
+				}
+
+				ann.Value.Params = append(ann.Value.Params, def)
+
+				if tok.PeekToken().Type == TypeComma {
+					tok.NextToken()
+				}
+			}
+
 		case String:
 			tok.NextToken()
 
@@ -59,10 +88,57 @@ func parseAnnotation(tok Tokenizer) (*AnnotationDef, error) {
 		}
 		tok.NextToken()
 	}
-
 	tok.NextToken()
 
 	return ann, nil
+}
+
+type assignmentDef struct {
+	Token
+	Var   string
+	Value AnnotationValue
+}
+
+func (a *assignmentDef) node() {}
+
+func (a *assignmentDef) definition() {}
+
+func parseAssignment(tok Tokenizer) (*assignmentDef, error) {
+	log.Println("Parse assignment var")
+	if tok.CurrentToken().Type != TypeIdent {
+		return nil, fmt.Errorf("expected <ident> but found %v", tok.CurrentToken())
+	}
+
+	def := &assignmentDef{
+		Token: tok.CurrentToken(),
+	}
+
+	log.Println("Parse assignment sign")
+	if err := advanceToken(tok, Assign); err != nil {
+		return nil, err
+	}
+
+	switch tok.PeekToken().Type {
+	case String:
+		tok.NextToken()
+
+		log.Println("Parse assignment string")
+		def.Value.String = StringPtr(tok.CurrentToken().Literal)
+	case Bool:
+		tok.NextToken()
+
+		log.Println("Parse assignment bool")
+		def.Value.Bool = StringPtr(tok.CurrentToken().Literal)
+	case Number:
+		tok.NextToken()
+
+		log.Println("Parse assignment number")
+		def.Value.Number = StringPtr(tok.CurrentToken().Literal)
+	default:
+		return nil, fmt.Errorf("expected one of <string|bool|number> %v", tok.CurrentToken())
+	}
+
+	return def, nil
 }
 
 func StringPtr(s string) *string {
